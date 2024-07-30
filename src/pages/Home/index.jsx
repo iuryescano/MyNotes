fe94dc32d37b412b51b40001e2580bc1b4bed95b
iuryescano <iurycordeiro15@gmail.com>
2024-07-30 16:56:24 -0300

    IsActive adding in ButtonText and Menu is created in Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,23 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
+
+
 
 export function Home(){
     return(
         <Container>
             <Brand>
-                <h1>RocketNotes</h1>
+                <h1>MyNotes</h1>
             </Brand>
 
         <Header/>
 
         <Menu>
-
+            <li><ButtonText title="Todos" /></li>
+            <li><ButtonText title="React" /></li>
+            <li><ButtonText title="Nodejs" /></li>
         </Menu>
             
         <Search>
