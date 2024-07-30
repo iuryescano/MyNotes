e6d573cde982bb7bc288643a248886b06fb58ec5
iuryescano <iurycordeiro15@gmail.com>
2024-07-30 11:05:58 -0300

    Adding Button Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,14 @@
 import { Container } from "./styles";
 
+import { Button } from "../../components/Button";
+
 export function Details() { //Se eu tivesse colocado default lá no main eu não precisaria colocar entre {} pois eu estaria exportando tudo
   return ( //1 componente sempre vai retornar sempre 1 único elemento por isso da div envolta de tudo
     <Container>
       <h1>Hello World!</h1>
+
+      <Button />
     </Container>
   )
-} 
+}
+ 
