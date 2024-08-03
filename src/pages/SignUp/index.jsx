f07f2fce9914454f6dbc6ab6e2b429945f33b1d7
iuryescano <iurycordeiro15@gmail.com>
2024-08-03 11:34:30 -0300

    SignUp Page Created and image ajusted

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import { FiMail, FiLock} from 'react-icons/fi';
+import { FiMail, FiLock, FiUser} from 'react-icons/fi';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -8,14 +8,21 @@ import { Container, Form, Background } from "./styles";
 
 
 
-export function SignIn(){
+export function SignUp (){
   return(
     <Container>
+      <Background />
       <Form>
         <h1>MyNotes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie sua Conta</h2>
+
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
 
         <Input
           placeholder="E-mail"
@@ -29,14 +36,14 @@ export function SignIn(){
           icon={FiLock}
         />
 
-        <Button title='Entrar'/>
+        <Button title='Cadastrar'/>
         <a href="#">
-          Criar Conta
+          Voltar para o login
         </a>
 
       </Form>
 
-      <Background />
+      
     </Container>
   );
 }
