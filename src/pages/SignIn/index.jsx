da44c31b6d17728b5614522bd50b698dd686eed1
iuryescano <iurycordeiro15@gmail.com>
2024-08-31 13:44:11 -0300

    :sparkles: feat: Adding auth hooks in project

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import { FiMail, FiLock} from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth'
+
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 import { Container, Form, Background } from "./styles";
 
 
-
-
 export function SignIn(){
-  return(
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+  // console.log("MEU CONTEXTO =>", data);
+
+  function handleSignIn(){
+    signIn({ email, password });
+  }
+
+  return (
     <Container>
       <Form>
         <h1>MyNotes</h1>
@@ -21,15 +33,17 @@ export function SignIn(){
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={event => setEmail(event.target.value)}
         />
 
         <Input
           placeholder="Senha"
-          type='password'
+          type="password"
           icon={FiLock}
+          onChange={event => setPassword(event.target.value)}
         />
 
-        <Button title='Entrar'/>
+        <Button title="Entrar" onClick={handleSignIn}/>
 
         <Link to="/register">
           Criar Conta
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { FiMail, FiLock, FiUser} from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
+
+import { api } from "../../services/index";
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -10,6 +13,33 @@ import { Container, Form, Background } from "./styles";
 
 
 export function SignUp (){
+  const [name, setName] = useState("");//primeiro valor dentro do parenteses é o meu estado inicial 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp(){
+    if(!name || !email || !password) {
+      alert("Preencha todos os campos");
+      return; //encerra a função se algum campo não for preenchido
+    }
+    //console.log(name, email, password);
+
+    api.post("/users", { name, email, password })
+    .then(() => {
+      alert("Conta criada com sucesso!");
+      navigate("/");
+    })
+    .catch( error => {
+      if(error.response){
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível cadastrar");
+      }
+    })
+  }
+
   return(
     <Container>
       <Background />
@@ -23,21 +53,24 @@ export function SignUp (){
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e=> setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type='password'
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title='Cadastrar'/>
+        <Button title='Cadastrar' onClick={handleSignUp}/>
         <Link to="/">
           Voltar para o login
         </Link>
